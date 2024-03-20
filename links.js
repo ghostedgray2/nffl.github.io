a0cd8a341799f3f66a6ffb6af375f478209d26e1
ufa-ff <129699207+ufa-ff@users.noreply.github.com>
2024-03-19 23:32:05 -0400

    Create links.js

---
+++ links.js
@@ -0,0 +1,9 @@
+  let Links =
+  {
+      home : "/league",
+      qbstats : "/league/stats/qb",
+      wrstats : "/league/stats/wr",
+      dbstats : "/league/stats/db",
+      destats : "/league/stats/de",
+      standings : "/league/stats/standings"
+  };
