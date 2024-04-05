b870be683aa0958ad2366e275c6dd1bb0dd47927
ufa-ff <129699207+ufa-ff@users.noreply.github.com>
2024-04-05 15:24:41 +0000

    agt

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use('',express.static(path.join(__dirname, 'public')));
+
 app.get('/', (request, response) => {
   return response.sendFile('index.html', { root: '.' });
 });
@@ -12,61 +13,6 @@ app.get('/', (request, response) => {
 app.get('/auth/discord', (request, response) => {
   return response.sendFile('dashboard.html', { root: '.' });
 });
+
 const port = '53134';
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
-
-window.onload = () => {
-const fragment = new URLSearchParams(window.location.hash.slice(1));
-const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
-
-if (!accessToken) {
-    window.location.href('/')
-    return (document.getElementById('login').style.display = 'block');
-}
-
-
-};
-fetch('https://discord.com/api/users/@me', {
-  headers: {
-      authorization: `${tokenType} ${accessToken}`,
-  },
-})
-.then(result => result.json())
-.then(response => {
-  //handle response
-})
-.catch(console.error);
-const { username, discriminator, avatar, id} = response;
-//set the welcome username string
-window.onload = () => {
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
-
-    if (!accessToken) {
-        window.location.href('/')
-    }
-if (accessToken) {
-  localStorage.setItem("username", ` ${username}#${discriminator}`)
-  localStorage.setItem("avatar", `https://cdn.discordapp.com/avatars/${id}/${avatar}.jpg`)
-}
-    fetch('https://discord.com/api/users/@me', {
-    headers: {
-        authorization: `${tokenType} ${accessToken}`,
-    },
-    })
-    .then(result => result.json())
-    .then(response => {
-        console.log(response);
-        const { username, discriminator, avatar, id} = response;
-
-        //set the welcome username string
-        document.getElementById('disname').innerText = localStorage.getItem("username");
-
-        //set the avatar image by constructing a url to access discord's cdn
-        document.getElementById("disavatar").src = localStorage.getItem("avatar");
-    })
-    .catch(console.error);
-
-
-};
- 
